247412cf5b9048913a4f39aa57467723e96711f2
jhelbrink <john.helbrink@gmail.com>
2018-09-07 10:55:35 +0200

    finished 2nd part of lab

--- Laboratory 1/indexer.py
+++ Laboratory 1/indexer.py
@@ -1,16 +1,31 @@
 from sys import argv
+import os
 import re
 import pickle
 
-file_name = argv[1]
-file_object  = open(file_name).read()
+def get_files(dir, suffix):
+    """
+    Returns all the files in a folder ending with suffix
+    :param dir:
+    :param suffix:
+    :return: the list of file names
+    """
+    files = []
+    for file in os.listdir(dir):
+        if file.endswith(suffix):
+            files.append(file)
+    return files
 
+dir = argv[1]
+file_names = get_files(dir, ".txt")
 matches = {}
-for match in re.finditer(r"\w+", file_object.lower()):
-    if match.group(0) in matches:
-        matches[match.group(0)].append(match.start())
-    else:
-        matches[match.group(0)] = [match.start()]
 
-file_name = file_name.replace(".txt", "")
-pickle.dump(matches, open(file_name + ".idx", "wb"))
+for file_name in file_names:
+    file_object  = open(dir+"/"+file_name).read()
+    for match in re.finditer(r"\w+", file_object.lower()):
+        if match.group(0) in matches:
+            matches[match.group(0)].append(match.start())
+        else:
+            matches[match.group(0)] = [match.start()]
+
+pickle.dump(matches, open(dir + ".idx", "wb"))
