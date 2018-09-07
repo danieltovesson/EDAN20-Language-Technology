f282550b1afa504c095de6e5a6baf7d3aab036b6
Daniel Tovesson <daniel.tovesson@me.com>
2018-09-07 12:42:27 +0200

    Finish representing document with tf-idf

--- Laboratory 1/indexer.py
+++ Laboratory 1/indexer.py
@@ -3,7 +3,7 @@ import os
 import re
 import pickle
 
-def get_files(dir, suffix):
+def getFiles(dir, suffix):
     """
     Returns all the files in a folder ending with suffix
     :param dir:
@@ -17,18 +17,52 @@ def get_files(dir, suffix):
     return files
 
 dir = argv[1]
-file_names = get_files(dir, ".txt")
+fileNames = getFiles(dir, ".txt")
 matches = {}
 
-for file_name in file_names:
-    file_object  = open(dir + "/" + file_name).read()
-    for match in re.finditer(r"\w+", file_object.lower()):
+def computeTF(wordDict):
+    tfDict = {}
+    bowCount = sum(wordDict.values())
+    for word, count in wordDict.items():
+        tfDict[word] = count/float(bowCount)
+    return tfDict
+
+def computeIDF(wordPositions, fileNames):
+    import math
+    idfDict = {}
+    N = len(fileNames)
+
+    for word, val in wordPositions.items():
+        idfDict[word] = math.log10(N / float(len(val)))
+
+    return idfDict
+
+def computeTFIDF(tf, idf):
+    tfidf = {}
+    for word, val in tf.items():
+        tfidf[word] = val*idf[word]
+    return tfidf
+
+fileWordDict = {}
+for fileName in fileNames:
+    fileObject  = open(dir + "/" + fileName).read()
+    fileWordDict[fileName] = {}
+    for match in re.finditer(r"\w+", fileObject.lower()):
         if match.group(0) in matches:
-            if file_name in matches[match.group(0)]:
-                matches[match.group(0)][file_name].append(match.start())
+            if fileName in matches[match.group(0)]:
+                matches[match.group(0)][fileName].append(match.start())
             else:
-                matches[match.group(0)][file_name] = [match.start()]
+                matches[match.group(0)][fileName] = [match.start()]
         else:
-            matches[match.group(0)] = {file_name: [match.start()]}
+            matches[match.group(0)] = {fileName: [match.start()]}
+        if match.group(0) not in fileWordDict[fileName]:
+            fileWordDict[fileName][match.group(0)] = 0
+        fileWordDict[fileName][match.group(0)] += 1
 
 pickle.dump(matches, open(dir + ".idx", "wb"))
+
+idf = computeIDF(matches, fileNames)
+for fileName in fileNames:
+    tf = computeTF(fileWordDict[fileName])
+    tfidf = computeTFIDF(tf, idf)
+    print(tfidf)
