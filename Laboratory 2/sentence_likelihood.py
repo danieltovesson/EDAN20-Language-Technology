3b8c6afe6ea2d9faa5e5672e0fcab9d34eae1707
jhelbrink <john.helbrink@gmail.com>
2018-09-13 14:16:10 +0200

    Added solution for unigrams

---
+++ Laboratory 2/sentence_likelihood.py
@@ -0,0 +1,40 @@
+def buildUnigramModel():
+    import sys
+    from count import tokenize, count_unigrams
+
+    text = sys.stdin.read().lower()
+    sentence = sys.argv[1].lower()
+
+    unigramModel = []
+
+    tokensText = tokenize(text)
+    tokensSentence = tokenize(sentence)
+
+    unigramCounts = count_unigrams(tokensText)
+    cw = []
+    numberOfWords = []
+    pw = []
+
+    for token in tokensSentence:
+        if token in unigramCounts:
+            cw.append(unigramCounts[token])
+        else:
+            cw.append(0)
+    for cwi in cw: numberOfWords.append(len(tokensText))
+    for cwi in cw: pw.append(cwi/numberOfWords[0])
+
+    unigramModel.append(tokensSentence)
+    unigramModel.append(cw)
+    unigramModel.append(numberOfWords)
+    unigramModel.append(pw)
+
+    return unigramModel
+
+print('Unigram model')
+print('=====================================================')
+print('wi C(wi) #words P(wi)')
+print('=====================================================')
+unigramModel = buildUnigramModel()
+
+for unigram in unigramModel:
+    print(unigram)
