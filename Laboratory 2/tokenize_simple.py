cec32e0d80ca5e497b0802f851c50818803541c4
Daniel Tovesson <daniel.tovesson@me.com>
2018-09-13 09:15:39 +0200

    Replace tokenizer, took the wrong one Add files for each step, Tokens, Sorted and Uniq

---
+++ Laboratory 2/tokenize_simple.py
@@ -0,0 +1,12 @@
+"""
+Elementary tokenizer
+Usage: python tokenize_simple.py < corpus.txt
+"""
+__author__ = "Pierre Nugues"
+
+import sys
+import re
+
+text = sys.stdin.read()
+text = re.sub('\s+', '\n', text)
+print(text, end='')
