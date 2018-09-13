8afee44cd676ac2cf4040aca0fbf09bf087631e9
jhelbrink <john.helbrink@gmail.com>
2018-09-13 10:41:45 +0200

    added stripping of text

--- Laboratory 2/tokenize_sentences.py
+++ Laboratory 2/tokenize_sentences.py
@@ -8,10 +8,19 @@ def findSentences():
     import sys
     import regex as re
     text = sys.stdin.read()
+    text = re.sub('\s+', ' ', text, flags = re.MULTILINE)
     matches = re.findall('([\p{Lu}][^.]*[.])', text)
     result = ''
     for match in matches:
-        result += '<s>' + match + '</s>\n'
+        result += '<s> ' + match + '</s>\n'
     return result
 
-print(findSentences(), end='')
+def stripSentences(text):
+    import regex as re
+    text = re.sub('\.+', ' ', text)
+    text = text.lower()
+    return text
+
+text = findSentences()
+text = stripSentences(text)
+print(text, end='')
