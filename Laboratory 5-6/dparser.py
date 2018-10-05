ea2f7a378fc07c35421b8e73550e461999cd664c
Daniel Tovesson <daniel.tovesson@me.com>
2018-10-05 13:56:16 +0200

    Start on lab 6

--- Laboratory 5-6/dparser.py
+++ Laboratory 5-6/dparser.py
@@ -13,6 +13,7 @@ from sklearn.feature_extraction import DictVectorizer
 from sklearn import linear_model
 #from sklearn.tree import DecisionTreeClassifier
 from sklearn import metrics
+from sklearn.externals import joblib
 
 def reference(stack, queue, graph):
     """
@@ -50,6 +51,11 @@ def reference(stack, queue, graph):
     stack, queue, graph = transition.shift(stack, queue, graph)
     return stack, queue, graph, 'sh'
 
+    def parse_ml(stack, queue, graph, trans):
+        if stack and trans[:2] == 'ra':
+            stack, queue, graph = transition.right_arc(stack, queue, graph, trans[3:])
+            return stack, queue, graph, 'ra'
+
 if __name__ == '__main__':
     train_file = 'datasets/swedish_talbanken05_train.conll'
     test_file = 'datasets/swedish_talbanken05_test_blind.conll'
@@ -61,6 +67,8 @@ if __name__ == '__main__':
 
     sentences = conll.read_sentences(train_file)
     formatted_corpus = conll.split_rows(sentences, column_names_2006)
+
+    vec = DictVectorizer(sparse=True)
 
     print("Extracting the features...")
     sent_cnt = 0
@@ -92,19 +100,23 @@ if __name__ == '__main__':
         #print(transitions)
         #print(graph)
 
-    # print("Encoding the features...")
-    # vec = DictVectorizer(sparse=True)
-    # X_fit_transform = vec.fit_transform(X)
-    #
-    # print("Training the model...")
-    # classifier = linear_model.LogisticRegression(penalty='l2', dual=True, solver='liblinear')
-    # model = classifier.fit(X_fit_transform, y)
-    # print(model)
-    #
-    # X_transform = vec.transform(X)
-    # y_predicted = classifier.predict(X_transform)
-    # print("Classification report for classifier %s:\n%s\n"
-    #       % (classifier, metrics.classification_report(y, y_predicted)))
+    modelFilename = 'model.sav'
+    try:
+        classifier = joblib.load(modelFilename)
+        print("Model found in memory")
+    except:
+        print("Encoding the features...")
+        X_fit_transform = vec.fit_transform(X)
+
+        print("Training the model...")
+        classifier = linear_model.LogisticRegression(penalty='l2', dual=True, solver='liblinear')
+        model = classifier.fit(X_fit_transform, y)
+        joblib.dump(model, modelFilename)
+
+        X_transform = vec.transform(X)
+        y_predicted = classifier.predict(X_transform)
+        print("Classification report for classifier %s:\n%s\n"
+              % (classifier, metrics.classification_report(y, y_predicted)))
 
     keys = ['stack0_POS', 'stack1_POS', 'stack0_word', 'stack1_word', 'queue0_POS', 'queue1_POS', 'queue0_word', 'queue1_word', 'can-re', 'can-la', 'stack_following_POS', 'stack_word_following_word', 'queue_following_POS', 'queue_following_word']
     for i in range(9):
@@ -112,6 +124,27 @@ if __name__ == '__main__':
         for key in keys:
             if key in X[i]:
                 x.append(X[i][key])
-        print("x = ", end='')
-        print(x)
-        print("y = " + y[i])
+        #print("x = ", end='')
+        #print(x)
+        #print("y = " + y[i])
+
+    sentences = conll.read_sentences(test_file)
+    formatted_corpus = conll.split_rows(sentences, column_names_2006_test)
+
+    for sentence in formatted_corpus:
+        stack = []
+        queue = list(sentence)
+        graph = {}
+        graph['heads'] = {}
+        graph['heads']['0'] = '0'
+        graph['deprels'] = {}
+        graph['deprels']['0'] = 'ROOT'
+        transitions = []
+        while queue:
+            X_features = features.extract(stack, queue, graph, feature_names_2, sentence)
+            X_transform = vec.transform(X_features)
+            trans_pred = classifier.predict(X_transform)
+            print(trans_pred)
+            trans = dict_classes[trans_pred[0]]
+            stack, queue, graph, trans = parse_ml(stack, queue, graph, trans)
+        stack, graph = transition.empty_stack(stack, graph)
